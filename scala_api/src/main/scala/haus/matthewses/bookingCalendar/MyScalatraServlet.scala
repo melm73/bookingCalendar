34d4f44a903ded21673a253cf6a0f492a9eda5b0
Melanie Matthews <melanie.a.matthews@gmail.com>
2016-09-11 21:39:46 +1000

    Add JSON handler

--- scala_api/src/main/scala/haus/matthewses/bookingCalendar/MyScalatraServlet.scala
+++ scala_api/src/main/scala/haus/matthewses/bookingCalendar/MyScalatraServlet.scala
@@ -1,14 +1,42 @@
 package haus.matthewses.bookingCalendar
 
-class MyScalatraServlet extends BookingcalendarStack {
+import org.scalatra._
+// JSON-related libraries
+import org.json4s.{DefaultFormats, Formats}
 
-  get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+// JSON handling support from Scalatra
+import org.scalatra.json._
+
+class MyScalatraServlet extends BookingcalendarStack with JacksonJsonSupport {
+
+
+  // Sets up automatic case class to JSON output serialization, required by
+  // the JValueResult trait.
+  protected implicit val jsonFormats: Formats = DefaultFormats
+//  protected implicit val jsonFormats: Formats = DefaultFormats.withBigDecimal
+
+  // Before every action runs, set the content type to be in JSON format.
+  before() {
+    contentType = formats("json")
   }
 
+  get("/"){
+    FlowerData.all
+  }
+
+}
+
+
+// A Flower object to use as a faked-out data model
+case class Flower(slug: String, name: String)
+
+object FlowerData {
+
+  /**
+    * Some fake flowers data so we can simulate retrievals.
+    */
+  var all = List(
+    Flower("yellow-tulip", "Yellow Tulip"),
+    Flower("red-rose", "Red Rose"),
+    Flower("black-rose", "Black Rose"))
 }
