9cc1c88cb6c2bc805e2bc182cd06231bdd06819e
Melanie Matthews <melanie.a.matthews@gmail.com>
2016-09-11 20:59:29 +1000

    Add scala api

---
+++ scala_api/README.md
@@ -0,0 +1,12 @@
+# bookingCalendar #
+
+## Build & Run ##
+
+```sh
+$ cd scala_api
+$ ./sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ scala_api/src/main/scala/haus/matthewses/bookingCalendar/MyScalatraServlet.scala
@@ -0,0 +1,14 @@
+package haus.matthewses.bookingCalendar
+
+class MyScalatraServlet extends BookingcalendarStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
