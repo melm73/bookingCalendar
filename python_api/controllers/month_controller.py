6a2d94d459e70af07a6992bea82a9fb3a2ce9695
Melanie Matthews <melanie.a.matthews@gmail.com>
2018-01-13 14:56:52 +1100

    python-api: change url structure

--- python_api/controllers/month_controller.py
+++ python_api/controllers/month_controller.py
@@ -2,23 +2,25 @@ import json
 import os
 import calendar
 
+
 from flask_restful import Resource
-from flask_restful import reqparse
+from werkzeug.exceptions import BadRequest
+# from flask_restful import reqparse
 from settings import APP_CALENDAR
 
 
 class MonthController(Resource):
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('month', type=int, required=True, choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-        parser.add_argument('year', type=int, required=True)
-        args = parser.parse_args()
-
+    def get(self, year, month):
+        self.validate_args(year, month)
         try:
-            return json.load(open(self.calendar_file_name(args['month'], args['year'])))
+            return json.load(open(self.calendar_file_name(month, year)))
         except FileNotFoundError:
             return {}
 
     def calendar_file_name(self, month, year):
         file_name = f"{year}_{calendar.month_name[month]}.json"
         return os.path.join(APP_CALENDAR, file_name)
+
+    def validate_args(self, year, month):
+        if month < 1 or month > 12:
+            raise BadRequest({ 'month': 'Month must be between 1 and 12' })
--- python_api/server.py
+++ python_api/server.py
@@ -5,7 +5,7 @@ from controllers.month_controller import MonthController
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(MonthController, '/months/')
+api.add_resource(MonthController, '/year/<int:year>/month/<int:month>')
 
 if __name__ == '__main__':
     app.run(debug=False, host='localhost', port=8080)
