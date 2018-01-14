903be16ebede003c6fbe23d1ee7c90993fb0d706
Melanie Matthews <melanie.a.matthews@gmail.com>
2018-01-14 22:04:36 +1100

    python-api: add script to run tests

--- python_api/run_tests.py
+++
@@ -1,5 +0,0 @@
-import unittest
-
-if __name__ == "__main__":
-    testsuite = unittest.TestLoader().discover('tests', pattern = '*_test.py')
-    unittest.TextTestRunner(verbosity=2).run(testsuite)
